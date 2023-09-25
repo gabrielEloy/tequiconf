e1aef96365cdd141fcad4dd5ad9bb6d6bd2ff03a
gabrielEloy <gabrielEloy>
2023-09-24 23:55:44 -0300

    removed unused console logs

--- src/components/PopUp/PopUp.tsx
+++ src/components/PopUp/PopUp.tsx
@@ -18,18 +18,6 @@ const PopUp = ({
   const bodyRef = useRef<HTMLDivElement>(null);
   const headerRef = useRef<HTMLDivElement>(null);
 
-  useLayoutEffect(() => {
-    const containerSize = containerRef?.current?.getBoundingClientRect();
-    const bodySize = bodyRef?.current?.getBoundingClientRect();
-    const headerSize = headerRef?.current?.getBoundingClientRect();
-
-    console.log({
-      containerSize: containerSize?.height,
-      bodySize: bodySize?.height,
-      headerSize: headerSize?.height,
-    });
-  }, []);
-
   const wrapperClassName = className
     ? `${styles["popup-window"]} ${className}`
     : styles["popup-window"];
