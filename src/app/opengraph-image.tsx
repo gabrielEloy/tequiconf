51c41bd88cc7bb3442f43bc2360ac9479a97f308
gabrielEloy <gabrielEloy>
2023-09-24 23:44:22 -0300

    opengrah

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,46 @@
+import { ImageResponse } from "next/server";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "About Acme";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image() {
+  // Font
+  const interSemiBold = fetch(
+    new URL("./Inter-SemiBold.ttf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <img
+        style={{ width: 1200, height: 630 }}
+        src={new URL("../public/open-graph.png", import.meta.url).toString()}
+        alt="Tequiconf"
+      />
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: await interSemiBold,
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    }
+  );
+}
