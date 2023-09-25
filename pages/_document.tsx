9d00e0c3f6c9f24cbb58a56972131f623c6c74c3
gabrielEloy <gabrielEloy>
2023-09-25 13:35:01 -0300

    vamo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,14 +15,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700&family=Poppins:ital,wght@0,500;0,600;0,900;1,900&display=swap"
           rel="stylesheet"
         />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=3536686016594795&ev=PageView&noscript=1`}
-          />
-        </noscript>
       </Head>
       <body>
         <Main />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,32 @@ import { Canvas } from "@/components/Canvas/Canvas";
 import { Speakers } from "@/components/Speakers/Speakers";
 import Info from "@/components/Info/Info";
 import { Tickets } from "@/components/Tickets/Tickets";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    const head = document.querySelector("head");
+    const pixelCode = `<!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '3536686016594795');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=3536686016594795&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->`;
+
+    const pixelNode = new DOMParser().parseFromString(pixelCode, "text/html");
+    head?.appendChild(pixelNode.documentElement);
+  }, []);
   return (
     <div className={styles.main}>
       <div className={styles["hero-container"]}>
